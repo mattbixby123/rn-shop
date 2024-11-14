3a575bd24b269e75e7737244b0cb0b2a344c73c2
mattbixby123 <matthew.bixby1@gmail.com>
2024-11-14 12:57:49 -0500

    404 pages handled properly with expo router +not-found.tsx file

--- src/components/list-header.tsx
+++ src/components/list-header.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'expo-router';
-import { Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, Image, Pressable, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'
+import { CATEGORIES } from '../../assets/categories';
 
 export const ListHeader = () => {
   return (
@@ -42,8 +43,32 @@ export const ListHeader = () => {
             </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.heroContainer}></View>
-        <View style={styles.categoriesContainer}></View>
+        <View style={styles.heroContainer}>
+          <Image
+            source={require('../../assets/images/hero.png')}
+            style={styles.heroImage}
+          />
+        </View>
+        <View style={styles.categoriesContainer}>
+          <Text style={styles.sectionTitle}>Categories</Text>
+          <FlatList
+            data={CATEGORIES}
+            renderItem={({ item }) => (
+              <Link asChild href={`/categories/${item.slug}`}>
+                <Pressable style={styles.category}>
+                  <Image
+                    source={{ uri: item.imageUrl }}
+                    style={styles.categoryImage}
+                  />
+                  <Text style={styles.categoryText}>{item.name}</Text>
+                </Pressable>
+              </Link>
+            )}
+            keyExtractor={item => item.name}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
       </View>
   )
 }
